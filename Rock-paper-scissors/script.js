bc1eea4cefb351d870bb7ca73dce996a6f092aed
FranciskaDendooven <franciska.dendooven@gmail.com>
2022-05-18 16:00:10 +0200

    WIP - Try out several possibilities

---
+++ Rock-paper-scissors/script.js
@@ -0,0 +1,20 @@
+const selectBtn = document.getElementsByClassName("selectBtn");
+const selectB = document.getElementsByClassName("selectB");
+
+const userChoice = userInput =>{
+
+};
+
+
+
+/* const randomNumber = Math.floor(Math.random() * 17) + 1; //generates a nr between 0 and 17. 17 is not included, so add '+1' to make it 17.
+console.log('Random Number', randomNumber);
+
+btn.addEventListener('click', function(){
+    let guess = document.getElementById("guess").value;
+    if (guess == randomNumber) {
+        alert(`Awesome! ${randomNumber} was correct. You can be named many things, hungry not being one of them.`);
+      } else if (guess !== randomNumber) {
+        alert(`Bummer... You guessed ${guess}. The secret number was ${randomNumber}.`);//use `` to avoid using signs like +, ""..
+      }
+}); */

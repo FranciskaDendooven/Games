2b3033cf01ce1a0d82f9869f57a6a17cf6e3df01
FranciskaDendooven <franciska.dendooven@gmail.com>
2022-05-19 11:21:35 +0200

    Wip - add replay button to game rock-paper-scissor

--- Rock-paper-scissors/script.js
+++ Rock-paper-scissors/script.js
@@ -13,48 +13,49 @@ clickedImg2=[`<img src="./img/paper.svg" alt="Black stylized paper with outcrop 
 `<img src="./img/rock.svg" alt="Black stylized rock with outcrop displaying: rock" id="img2" width="200" height="200">`,
 `<img src="./img/scissor.svg" alt="Black stylized scissor with outcrop, twice displaying: scissor" id="img2" width="200" height="200">`];
 
+
 function game(position){
     document.getElementById("img1").style.animation="shakeLeft 1s 5 ease-in-out";
     document.getElementById("img2").style.animation="shakeRight 1s 5 ease-in-out";
 
-    const random = Math.floor(Math.random() * 3); //generates a nr between 0 and 17. 17 is not included, so add '+1' to make it 17.
+    const random = Math.floor(Math.random() * 3);
     console.log('Random', random);
     setTimeout(() => {
         document.getElementById("player2").innerHTML=clickedImg2[random];
         document.getElementById("player1").innerHTML=clickedImg1[position];
-    }, 5000);
+    }, 5500);
+    console.log(game);
 
     setTimeout(() => {
         if (random == position){
             feedback.innerHTML = "DRAW!";
+            document.getElementById("feedback").setTimeout();
             window.location.reload();
         }
         else if(position == 0 && random == 2){
             feedback.innerHTML = "PLAYER WON!";
+            document.getElementById("feedback").setTimeout();
             window.location.reload();
         }
         else if(position == 1 && random == 0){
             feedback.innerHTML = "PLAYER WON!";
+            document.getElementById("feedback").setTimeout();
             window.location.reload();
         }
         else if(position == 2 && random == 1){
             feedback.innerHTML = "PLAYER WON!";
+            document.getElementById("feedback").setTimeout();
             window.location.reload();
         }
         else{
             feedback.innerHTML = "COMPUTER WON! Better Luck Next Time!";
+            document.getElementById("feedback").setTimeout();
         }
     }, 5500);
+
+    advance();
 }
 
-/* const randomNumber = Math.floor(Math.random() * 17) + 1; //generates a nr between 0 and 17. 17 is not included, so add '+1' to make it 17.
-console.log('Random Number', randomNumber);
-
-btn.addEventListener('click', function(){
-    let guess = document.getElementById("guess").value;
-    if (guess == randomNumber) {
-        alert(`Awesome! ${randomNumber} was correct. You can be named many things, hungry not being one of them.`);
-      } else if (guess !== randomNumber) {
-        alert(`Bummer... You guessed ${guess}. The secret number was ${randomNumber}.`);//use `` to avoid using signs like +, ""..
-      }
-}); */
+function replay(){
+    document.getElementById("endScreen").style.display = 'none';
+}

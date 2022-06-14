c0db57754a7134ad32a485bf98a07b165e3ae2c4
FranciskaDendooven <franciska.dendooven@gmail.com>
2022-06-14 15:09:19 +0200

    Wip: start-over dummy memorygame

--- Memory-game/collection.js
+++ Memory-game/collection.js
@@ -1,4 +1,23 @@
 const cardList = [
-"alles alle", "alone", "antifa", "freedom", "freedom here"];
+    {
+        img: "/Memory-game/img/alles alle.jpg"
+    },
+    {
+        img: "/Memory-game/img/alone.jpg"
+    },
+    {
+        img: "/Memory-game/img/antifa.jpg"
+    },
+    {
+        img: "/Memory-game/img/freedom here.jpg"
+    },
+    {
+        img: "/Memory-game/img/freedom.jpg"
+    },
+    {
+        imgBack: "/Memory-game/img/play.jpg"
+    }
+/* "alles alle", "alone", "antifa", "freedom", "freedom here" */
+];
 
 export{cardList};

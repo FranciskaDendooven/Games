b68d0b3bf96e79e619ea8ab7c9a42fcb1c76d0d1
FranciskaDendooven <franciska.dendooven@gmail.com>
2022-06-16 15:26:29 +0200

    Wip and transfer memory card game to repository: games

--- Memory-game/script.js
+++ Memory-game/script.js
@@ -1,48 +1,47 @@
 import {cardList, cardBg} from './collection.js';
-//Take what you need!
-const section = document.querySelector("section");
-const playerLivesCount = document.querySelector("span");
-const playerLives = 6;
 
-//link text
-playerLivesCount.textContent = playerLives;
+const cards = document.querySelectorAll('.card');
 
-//randomize
 const randomize = () => {
     const cardData = cardList;
     cardData.sort(() => Math.random() - 0.5);
-    // console.log(cardData);
+    //console.log(cardData);
     return cardData;
 };
 
-//card generator function
 const cardGenerator = () => {
     const cardData = randomize();
    //generate html
-    cardList.forEach(card => {
-        // console.log(card.img);
-        const cardBody = document.createElement("div");
-        const face = document.createElement("img");
-        const back = document.createElement("img");
-        cardBody.classList = "cardBody";
-        face.classList = "face";
-        back.classList = "back";
-
-        // console.log("face:" + back.classList);
-        // console.log("back:" + back.classList);
-
-        face.src = card.img;
+        //     cardList.forEach(card => {
+//         console.log(card.img);
+//         const cardBody = document.querySelector(".card");
+//         const face = document.querySelector(".card__face--front");
+//         const back = document.querySelector(".card__face--back");
+//      console.log("cow");
+        /* face.src = card.img;
 
         cardBg.forEach(card =>{
             back.src = card.img;
-        });
-        
+        }); */
 
-        section.append(cardBody);
-        cardBody.append(face);
+        /* cardBody.append(face);
         cardBody.append(back);
+        console.log("horse"); */
 
-    });
+        cards.forEach((card, index)=>{
+            // const cardChild = card.firstElementChild;
+            const firstChild = card.firstElementChild;
+            const lastChild = card.lastElementChild;
+
+            // console.log(cardChild.firstChild);
+            firstChild.firstChild.src = cardList[index].img;
+            lastChild.firstChild.src = cardBg[0].img;
+            // console.log(cardList[index]);
+
+            card.addEventListener('click', () => {
+                card.classList.toggle('is-flipped');
+            });
+        });
 
 };
 cardGenerator();

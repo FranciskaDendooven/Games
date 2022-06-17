278ed0f9292929d69aeeb305ba66f821453fb0c5
FranciskaDendooven <franciska.dendooven@gmail.com>
2022-06-17 10:48:00 +0200

    Match cards and reset after completing the game

--- Memory-game/script.js
+++ Memory-game/script.js
@@ -1,6 +1,7 @@
 import {cardList, cardBg} from './collection.js';
 
 const cards = document.querySelectorAll('.card');
+let matchedCards = 0;
 
 const randomize = () => {
     const cardData = cardList;
@@ -11,22 +12,6 @@ const randomize = () => {
 
 const cardGenerator = () => {
     const cardData = randomize();
-   //generate html
-        //     cardList.forEach(card => {
-//         console.log(card.img);
-//         const cardBody = document.querySelector(".card");
-//         const face = document.querySelector(".card__face--front");
-//         const back = document.querySelector(".card__face--back");
-//      console.log("cow");
-        /* face.src = card.img;
-
-        cardBg.forEach(card =>{
-            back.src = card.img;
-        }); */
-
-        /* cardBody.append(face);
-        cardBody.append(back);
-        console.log("horse"); */
 
         cards.forEach((card, index)=>{
             // const cardChild = card.firstElementChild;
@@ -38,10 +23,55 @@ const cardGenerator = () => {
             lastChild.firstChild.src = cardBg[0].img;
             // console.log(cardList[index]);
 
-            card.addEventListener('click', () => {
+            firstChild.setAttribute("name", cardList[index].name);
+
+            card.addEventListener('click', (e) => {
                 card.classList.toggle('is-flipped');
+                checkCards(e);
             });
         });
+        const checkCards = (e) => {
+            const clickedCard = e.target;
+            clickedCard.classList.add("flipped");
+            const flippedCards = document.querySelectorAll(".flipped:not(.matched)");//controle 'geen match' : => om if function te laten werken
+            console.log(flippedCards);
+            if(flippedCards.length === 2 ){
+                if (flippedCards[0].innerHTML === flippedCards[1].innerHTML){
+                    console.log("match");
+                    flippedCards[0].classList.add('matched');
+                    flippedCards[1].classList.add('matched');
+                    ++matchedCards;//+1 als het een match is
+
+                    if(matchedCards==8){
+                        console.log('itsAMatch!');
+                        document.querySelector("#winner").style.visibility = "visible";
+                    }
+
+                } else {
+                    console.log("wrong");
+                    setTimeout(() => { flipCardsBack(); }, 1000);
+
+                const flipCardsBack = () => {
+                    flippedCards[0].classList.remove('flipped');
+                    flippedCards[0].classList.toggle('is-flipped');
+                    flippedCards[1].classList.remove('flipped');
+                    flippedCards[1].classList.toggle('is-flipped');
+                };
+                }
+            }
+        };
 
 };
 cardGenerator();
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+const endCardGame = document.querySelectorAll(".matched");
+endCardGame.forEach(card => {
+    card.classList.remove("flipped");
+    card.classList.remove("matched");
+    card.classList.toggle('is-flipped');
+});
+document.querySelector("#winner").style.visibility = "hidden";
+matchedCards = 0;
+});
